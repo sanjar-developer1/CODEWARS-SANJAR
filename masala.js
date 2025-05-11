c7917cfd545a9045581f53410c14c3883127af39
sanjarcik <sanjarbekarabboyev22@gmail.com>
2025-05-11 13:20:43 +0500

    Bugun 2 kun

---
+++ masala.js
@@ -0,0 +1,58 @@
+// Masala 1
+
+// let arr = [1, 2, 3, 4, 6, 7, 8, 9];
+// let result = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] + 2 == arr[i + 1]) {
+//     result = arr[i] + 1;
+//   }
+// }
+// console.log(result);
+
+// Masala 2
+
+// let arr = [1, 2, 3, 4, 6, 7, 8, 9];
+// let newarr = [];
+// let result = 0;
+// let result2 = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] + 2 == arr[i + 1]) {
+//     result = arr[i] + 1;
+//     result2 = result + 1;
+//     newarr.push(result, result2);
+//   }
+// }
+// console.log(newarr);
+
+//  Masala 3
+
+// function doubleChar(str) {
+//   let reuslt = "";
+
+//   for (let i of str) {
+//     reuslt += i + i;
+//   }
+//   return reuslt;
+// }
+
+// console.log(doubleChar("Sanjarbek"));
+
+// Masala 4
+
+// var countSheep = function (num) {
+//   let result = "";
+//   for (let i = 1; i <= num; i++) {
+//     result += `${i} sheep...`;
+//   }
+//   return result;
+// };
+// console.log(countSheep(3));
+
+// function Ketti(str) {
+//   return str
+//     .split(" ")
+//     .map((harf) => harf.charAt(0).toUpperCase() + harf.slice(1))
+//     .join(" ");
+// }
+
+// console.log(Ketti("salom mening ismim sanjarbek"));
--- sharh.md
+++ sharh.md
@@ -1,2 +1,5 @@
 Masala 1: array orasida bitta tushib qolgan sonni topish
-Masala 2:  array orasida bitta tushib qolgan sonni topish va undan keyingi sonni qoshib yangi array qaytarish
+Masala 2: array orasida bitta tushib qolgan sonni topish va undan keyingi sonni qoshib yangi array qaytarish
+Masala 3: Agar satr berilgan bo'lsa, siz har bir belgi (katta-katta) bir marta takrorlanadigan satrni qaytarishingiz kerak.
+Masala 4: Manfiy bo'lmagan butun son berilgan bo'lsa, 3masalan, shivirlangan satrni qaytaring: "1 sheep...2 sheep...3 sheep...". Kirish har doim haqiqiy bo'ladi, ya'ni manfiy butun sonlar bo'lmaydi.
+Masala 5: Karochi bitta string keladi uni ichida gap bo'ladi. Bizni vazifa har bir so'zni bosh harifni katta qilish kerak
